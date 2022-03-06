f9e3e1aadafa42a1f0cd0f2df23e8383fbaa474d
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-06 20:12:52 +0530

    Time: 261 ms (5.04%), Space: 32.9 MB (12.14%) - LeetHub

---
+++ 414-third-maximum-number/414-third-maximum-number.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        min_value = -np.inf
+        max1 = max2 = res = min_value
+
+        for i in nums:
+            if i > max1:
+                max1, max2, res = i, max1, max2
+            elif max2 < i < max1:
+                max1, max2, res = max1, i, max2
+            elif res < i < max2:
+                res = i
+
+        if res != min_value:
+            return res
+        else:
+            return max1
