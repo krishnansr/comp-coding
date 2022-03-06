9f2d79be8f25b9cada67ebf07e0ca6cfe51d0917
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-06 20:39:18 +0530

    Time: 53 ms (65.31%), Space: 14 MB (83.15%) - LeetHub

---
+++ 415-add-strings/415-add-strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        res = ""
+        carry = 0
+
+        len1 = len(num1)
+        len2 = len(num2)
+        max_len = max(len1, len2)
+        for i in range(1, max_len+1):
+            digit1 = 0 if i > len1 else num1[-i]
+            digit2 = 0 if i > len2 else num2[-i]
+
+            sum = int(digit1) + int(digit2) + carry
+            carry, digit = sum // 10, sum % 10
+            res = str(digit) + res
+
+        if carry:
+            res = '1' + res
+        return res
