e5a3240f716aa480e13e98d90c6225c50299b595
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-07 14:11:20 +0530

    Time: 140 ms (84.84%), Space: 15.5 MB (29.68%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -2,6 +2,7 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         return reduce(lambda i, j: i^j, nums + list(range(len(nums)+1)), 0)
     
+    
     def missingNumber_formula_method(self, nums: List[int]) -> int:
         num_range = len(nums)
         return (num_range*(num_range+1))//2 - sum(nums)
