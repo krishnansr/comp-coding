c0ca0806a5b58d3fc2fa73889a78879a359d7b69
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-07 14:11:54 +0530

    Time: 133 ms (89.40%), Space: 15.3 MB (59.75%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -2,7 +2,6 @@ class Solution:
     def missingNumber_bitwise(self, nums: List[int]) -> int:
         return reduce(lambda i, j: i^j, nums + list(range(len(nums)+1)), 0)
     
-    
     def missingNumber(self, nums: List[int]) -> int:
         num_range = len(nums)
         return (num_range*(num_range+1))//2 - sum(nums)
