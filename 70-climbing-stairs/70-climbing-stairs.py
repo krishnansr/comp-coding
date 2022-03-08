ef66f72934ec8573bebbf8589527129b3b834bf6
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-08 20:10:32 +0530

    Time: 28 ms (93.64%), Space: 13.9 MB (76.66%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,7 +1,19 @@
 class Solution:
-    @lru_cache(maxsize=50)
     def climbStairs(self, n: int) -> int:
         if n < 4:
             return n
-        
+
+        prev = 0
+        fib = 1
+        for _ in range(n):
+            _sum = prev + fib
+            prev = fib
+            fib = _sum
+        return fib
+            
+    
+    # @lru_cache(maxsize=50)
+    def climbStairs_cached(self, n: int) -> int:
+        if n < 4:
+            return n
         return self.climbStairs(n-1) + self.climbStairs(n-2)
