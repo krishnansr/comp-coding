83c8f5671e95a172260920db9d7ae7924ef8c653
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-10 00:35:30 +0530

    Time: 122 ms (56.09%), Space: 20.8 MB (38.89%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -2,13 +2,11 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         ans = [0]
         
-        even = False
         _append = ans.append  # for time imp.
         for i in range(1, n + 1):
-            if even:
+            if i % 2 == 0:
                 _append(ans[i // 2])
             else:
                 _append(ans[-1] + 1)
-            even = ~even
             
         return ans
