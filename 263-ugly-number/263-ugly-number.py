232b8220b48f290be6a8bf8d1535fe7ae1710420
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-24 21:15:32 +0530

    Time: 47 ms (50.65%), Space: 13.8 MB (66.19%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n < 1:
+            return False  # negative nunmbers have -1 and 0 are not ugly
+        
+        for factor in [10, 9, 8, 6, 5, 4, 3, 2]:
+            while n % factor == 0:
+                n /= factor
+                
+        return n == 1
