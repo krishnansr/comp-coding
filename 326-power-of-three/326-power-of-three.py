481ba5d59de1e9da29b6bb1b3f7ce3d1e1ea5691
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-24 21:40:02 +0530

    Time: 117 ms (53.66%), Space: 13.9 MB (65.71%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -1,6 +1,10 @@
 class Solution:
+    max_pow = 3 ** 20
     def isPowerOfThree(self, n: int) -> bool:
-        
+        return n > 0 and self.max_pow % n == 0
+    
+    
+    def isPowerOfThree_log(self, n: int) -> bool:
         if n < 1:
             return False
         return (log10(n) / log10(3)).is_integer()
