309cf3943a1df1255511f25a547cdf00a37dc3b3
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-25 12:12:30 +0530

    Time: 84 ms (38.96%), Space: 13.9 MB (61.82%) - LeetHub

---
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
@@ -0,0 +1,16 @@
+class Solution:
+    def _update_dist(self, s, c, short_dist, direction):
+        _dist = len(s)
+        for i in range(_dist)[::direction]:
+            if s[i] == c:
+                _dist = 0
+            short_dist[i] = min(short_dist[i], _dist)
+            _dist += 1
+    
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        short_dist = [len(s) - 1] * len(s)
+        
+        self._update_dist(s, c, short_dist, direction=1)
+        self._update_dist(s, c, short_dist, direction=-1)
+
+        return short_dist
