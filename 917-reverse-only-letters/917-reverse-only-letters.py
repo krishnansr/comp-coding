08d21f5e7481c0ef9c16ed1dc67c0751aedaa6e2
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-26 18:25:19 +0530

    Time: 42 ms (53.72%), Space: 13.9 MB (21.18%) - LeetHub

--- 917-reverse-only-letters/917-reverse-only-letters.py
+++ 917-reverse-only-letters/917-reverse-only-letters.py
@@ -9,8 +9,8 @@ class Solution:
                 i += 1
             while i < j and str_s[j].lower() not in self._ascii:
                 j -= 1
-            
             str_s[i], str_s[j] = str_s[j], str_s[i]
             i += 1
             j -= 1
+        
         return ''.join(str_s)
