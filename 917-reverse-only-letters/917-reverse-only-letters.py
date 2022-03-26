9e4e3cf7ba7e8b2358814065f3cb2173b73c4d60
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-26 18:27:30 +0530

    Time: 28 ms (95.86%), Space: 13.9 MB (21.18%) - LeetHub

--- 917-reverse-only-letters/917-reverse-only-letters.py
+++ 917-reverse-only-letters/917-reverse-only-letters.py
@@ -1,13 +1,12 @@
 class Solution:
-    _ascii = set(string.ascii_lowercase)
     
     def reverseOnlyLetters(self, s: str) -> str:
         str_s = list(s)
         i, j = 0, len(s) - 1
         while i < j:
-            while i < j and str_s[i].lower() not in self._ascii:
+            while i < j and not str_s[i].isalpha():
                 i += 1
-            while i < j and str_s[j].lower() not in self._ascii:
+            while i < j and not str_s[j].isalpha():
                 j -= 1
             str_s[i], str_s[j] = str_s[j], str_s[i]
             i += 1
