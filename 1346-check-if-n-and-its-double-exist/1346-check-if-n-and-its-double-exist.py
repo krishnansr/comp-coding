e9fe0993d3e722ac247e3e72f2464f8ed0fc82f9
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-26 20:06:09 +0530

    Time: 59 ms (81.81%), Space: 13.9 MB (92.38%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -2,23 +2,8 @@ class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         pair_arr = [0] * 2001
         for i in arr:
-            if i > 0:
-                if pair_arr[2 * i] == 1:
-                    return True
-                if not i & 1:
-                    if pair_arr[i // 2] == 1:
-                        return True
-                pair_arr[i] = 1
-            elif i < 0:
-                i = abs(i)
-                if pair_arr[2 * i] == 2:
-                    return True
-                if not i & 1:
-                    if pair_arr[i // 2] == 2:
-                        return True
-                pair_arr[i] = 2
-            else:
-                pair_arr[0] += 1
-                if pair_arr[0] > 1:
-                    return True
+            _match = (i >= 0) - (i < 0)
+            if pair_arr[2 * i] == _match or (not i & 1 and pair_arr[i // 2] == _match):
+                return True
+            pair_arr[i] = _match
         return False
