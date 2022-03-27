cdf0321d89bb0292e85ac00867079a41b514553f
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-27 11:41:49 +0530

    Time: 591 ms (19.96%), Space: 15 MB (6.47%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.py
+++ 485-max-consecutive-ones/485-max-consecutive-ones.py
@@ -1,3 +1,4 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        # get contiguous block, split into zeros, then max len
         return max(map(len, ''.join(map(str, nums)).split('0')))
