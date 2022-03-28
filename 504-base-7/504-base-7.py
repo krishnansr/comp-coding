27e41e2976bcf1264ef7d6e379355f0a331a09b0
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-03-28 23:39:03 +0530

    Time: 35 ms (74.11%), Space: 13.9 MB (15.32%) - LeetHub

--- 504-base-7/504-base-7.py
+++ 504-base-7/504-base-7.py
@@ -1,6 +1,7 @@
 class Solution:
     def convertToBase7(self, num: int) -> str:
         sign, num, res = num < 0, abs(num), ''
+        
         while num:
             res = str(num % 7) + res
             num //= 7
