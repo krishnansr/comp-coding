f7bdfa321ba551743963bdd8912ba13e4b89fa5f
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-04-18 13:26:59 +0530

    Time: 85 ms (89.35%), Space: 14.1 MB (9.38%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.py
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.py
@@ -1,3 +1,23 @@
 class Solution:
+    @staticmethod
+    def isValid(x1, arr2, d):
+        l, r = 0, len(arr2) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if abs(arr2[mid] - x1) <= d:
+                return False
+            elif x1 < arr2[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return True
+    
     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        arr2.sort()
+        
+        return sum([self.isValid(x1, arr2, d) for x1 in arr1])
+    
+    
+    def findTheDistanceValueBrute(self, arr1: List[int], arr2: List[int], d: int) -> int:
         return sum([all([ abs(x1 - x2) > d for x2 in arr2]) for x1 in arr1])
