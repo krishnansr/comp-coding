20969f6fe0d317306cd6227521e7d69020950638
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-04-19 12:30:54 +0530

    Time: 850 ms (65.69%), Space: 27.9 MB (81.00%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -5,10 +5,7 @@ class Solution:
         max_sum = nums[0]
         curr_sum = 0
         for n in nums:
-            if curr_sum < 0:
-                curr_sum = 0
-            
+            curr_sum = max(0, curr_sum)
             curr_sum += n
             max_sum = max(max_sum, curr_sum)
-        
         return max_sum
