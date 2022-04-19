ee2f2e927ed5ce3461f4e478450527691022260d
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-04-19 11:39:29 +0530

    Time: 103 ms (56.92%), Space: 14.2 MB (15.75%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.py
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.py
@@ -1,7 +1,6 @@
 class Solution:
     def kthDistinct(self, arr: List[str], k: int) -> str:
         dist_map = {}
-        
         for word in arr:
             dist_map[word] = dist_map.get(word, 0) + 1
         
@@ -10,6 +9,7 @@ class Solution:
                 k -= 1
             if k == 0:
                 return word
+            
         return ''
         
         
