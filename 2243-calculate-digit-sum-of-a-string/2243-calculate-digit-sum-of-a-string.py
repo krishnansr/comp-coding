842066dbbb39ed6220f16ea9d7eb2fd907bbbbc8
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-04-19 12:02:39 +0530

    Time: 31 ms (87.49%), Space: 13.9 MB (66.84%) - LeetHub

--- 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.py
+++ 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.py
@@ -2,4 +2,5 @@ class Solution:
     def digitSum(self, s: str, k: int) -> str:
         while len(s) > k:
             s = ''.join(map(str, [sum(map(int, s[i: i+k])) for i in range(0, len(s), k)]))
+        
         return s
