1a26bd756203df6afc26c02feb93b24f0e2683da
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-04-20 17:25:30 +0530

    Time: 109 ms (13.63%), Space: 13.9 MB (66.50%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -1,3 +1,9 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
+        _str = [''] * len(indices)
+        for c, i in zip(s, indices):
+            _str[i] = c
+        return ''.join(_str)
+    
+    def restoreString_sorted(self, s: str, indices: List[int]) -> str:
         return ''.join([_char for _, _char in sorted(zip(indices, s))])
