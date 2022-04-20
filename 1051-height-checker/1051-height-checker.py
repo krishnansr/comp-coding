119b25ff15641509529156a11f587c92d0260490
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-04-20 17:54:44 +0530

    Time: 47 ms (58.91%), Space: 13.9 MB (12.10%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,3 +1,20 @@
 class Solution:
-    def heightChecker(self, heights: List[int]) -> int:
+    def heightCheckerr(self, heights: List[int]) -> int:
         return sum([x != y for x, y in zip(heights, sorted(heights))])
+    
+    def heightChecker(self, heights: List[int]) -> int:
+        # using count sort - O(n)
+        
+        cdf_freq = [0] * max(heights)
+        for h in heights:
+            cdf_freq[h-1] += 1
+        for i in range(1, len(cdf_freq)):
+            cdf_freq[i] += cdf_freq[i-1]
+        
+        cnt = 0
+        for h in heights:
+            if heights[cdf_freq[h-1]-1] != h:
+                cnt += 1
+            cdf_freq[h-1] -= 1
+        
+        return cnt
