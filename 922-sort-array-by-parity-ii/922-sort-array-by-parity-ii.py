48d4bb5e83ced3c9437ba3ecf013658dda05a04e
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-04-22 20:05:11 +0530

    Time: 336 ms (27.03%), Space: 16.2 MB (92.69%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
@@ -12,15 +12,3 @@ class Solution:
                     i += 2
                     j += 2
         return nums
-    
-    def sortArrayByParityII_search(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            if i & 1 == nums[i] & 1:
-                continue
-
-            j = len(nums) - 1
-            while nums[j] & 1 == nums[i] & 1:
-                j -= 1
-            nums[i], nums[j] = nums[j], nums[i]
-
-        return nums
