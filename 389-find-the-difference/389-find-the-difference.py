734527bf68806d37c5afac347876912a47c4ab1c
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-04-22 10:23:00 +0530

    Time: 40 ms (74.33%), Space: 13.8 MB (78.41%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,5 +1,12 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
+        res = 0
+        for _char in s  + t:
+            res ^= ord(_char)
+        return chr(res)
+        
+        
+    def findTheDifference_array(self, s: str, t: str) -> str:
         count = [0] * 26
         for _char in s:
             count[ord(_char) - 97] += 1
