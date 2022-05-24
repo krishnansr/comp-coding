5d3a070409c629985f53be9aa8141a198b1dc070
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-05-25 00:36:10 +0530

    Time: 355 ms (32.07%), Space: 16.3 MB (51.62%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -1,6 +1,20 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        pos_list, neg_list = [], []
+        res = [0] * len(nums)
+        l, r, i = 0, len(nums) - 1, len(nums) - 1
+        while l <= r:
+            left_val, right_val = abs(nums[l]), abs(nums[r])
+            if left_val > right_val:
+                res[i] = left_val ** 2
+                l += 1
+            else:
+                res[i] = right_val ** 2
+                r -= 1
+            i -= 1  # to fill next biggest value
+        return res
+    
+    def sortedSquares_extraspace(self, nums: List[int]) -> List[int]:
+        pos_list, neg_list = [], []  # extra O(n) space
         for n in nums:
             if n < 0:
                 neg_list.insert(0, n * n)
