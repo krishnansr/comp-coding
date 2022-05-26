f262df49cfb5038166d6db7ea76ee395a4ec60cb
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-05-26 18:26:07 +0530

    Time: 1519 ms (29.20%), Space: 21.4 MB (60.51%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
@@ -2,5 +2,4 @@ class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
         if len(original) != m * n:
             return []
-
         return [original[n * x : n * x + n] for x in range(m)]
