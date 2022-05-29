a52a59200e969f8c7aac14fb797e9f4298cf28dc
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-05-29 09:43:17 +0530

    lnls init

--- leetcode/scratch_linkls.py
+++ leetcode/scratch_linkls.py
@@ -5,29 +5,84 @@ from math import *
 from collections import OrderedDict
 
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, next=None, prev=None):
         self.data = data
-        self.next = None
-        self.prev = None
+        self.next = next
+        self.prev = prev
+
+    def __repr__(self):
+        return self.data.__repr__()
 
 
 class LinkedList:
+    def __init__(self, dataset=None):
+        self.head = None
+        self.size = 0
 
-    def count_nodes(self, head):
-        count = 1
-        while head.next is not None:
+        if dataset is not None:
+            _node = Node(dataset.pop(0))
+            self.head = _node  # handle head node separately as you need to keep track of it
+            self.size += 1
+            for elem in dataset:
+                _node.next = Node(elem)
+                _node = _node.next
+                self.size += 1
+
+    def __iter__(self):
+        _node = self.head
+        while _node is not None:
+            yield _node
+            _node = _node.next
+
+    def __repr__(self):
+        return ' -> '.join(map(str, self.__iter__()))
+
+    def length(self):
+        count = 0
+        for _node in self.__iter__():
             count += 1
-            head = head.next
         return count
 
-    def delete_node(self, node):
+    def append(self, data):
         pass
+
+    def appendleft(self, data):
+        _node = Node(data=data, next=self.head)
+        self.head = _node
+
+    def extend(self, dataset):
+        pass
+
+    def insert(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def popleft(self, node):
+        pass
+
+    def remove(self):
+        pass
+
 
 class Solution:
     def fn(self):
         pass
 
+
 if __name__ == '__main__':
     s = Solution()
     # print(s.construct2DArray(original = [1,1,1,1], m = 1, n = 4))
 
+    ls = LinkedList([1, 2, 3, 4])
+    print(ls)
+
+    ls.appendleft(0)
+    print(ls)
+
+    ls.append(99)
+    print(ls)
+
+
+    print()
