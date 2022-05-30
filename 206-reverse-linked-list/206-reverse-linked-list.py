820c1de4fe76aac9d028327a2c1e0033729777d0
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-05-30 10:41:38 +0530

    Time: 75 ms (8.19%), Space: 15.4 MB (56.16%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -4,18 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, node: Optional[ListNode]) -> Optional[ListNode]:
-        # for understadning recusive reversal: 
-        # https://www.youtube.com/watch?v=MRe3UsRadKw
-        if node is None or node.next is None:
-            return node
-        
-        node1 = self.reverseList(node.next)
-        node.next.next = node
-        node.next = None
-        return node1
-        
-    def reverseList_itr(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
         _node = head
         while _node is not None:
@@ -25,3 +14,15 @@ class Solution:
             _node = temp_next
         head = prev
         return head
+    
+    def reverseList_recursive(self, node: Optional[ListNode]) -> Optional[ListNode]:
+        # for understadning recusive reversal: 
+        # https://www.youtube.com/watch?v=MRe3UsRadKw
+        if node is None or node.next is None:
+            return node
+        
+        node1 = self.reverseList_recursive(node.next)
+        node.next.next = node
+        node.next = None
+        return node1
+    
