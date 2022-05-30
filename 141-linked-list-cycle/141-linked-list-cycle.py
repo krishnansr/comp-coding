76236b21b0b676705b4caaca6aeca78247515e24
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-05-30 23:28:47 +0530

    Time: 90 ms (29.70%), Space: 17.6 MB (66.24%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -6,6 +6,18 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # floyd's tortoise and hare algorithm
+        # explanation: https://www.youtube.com/watch?v=gBTe7lFR3vc&list=WL&index=2
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+        
+        
+    def hasCycle_value_change(self, head: Optional[ListNode]) -> bool:
         max_val = 100000
         _node = head
         while _node:
