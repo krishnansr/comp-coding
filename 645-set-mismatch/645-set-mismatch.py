7d0d6daddf204efaa5070afa1a6772ab0791c270
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-06-03 17:48:25 +0530

    Time: 208 ms (88.03%), Space: 16 MB (19.11%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.py
+++ 645-set-mismatch/645-set-mismatch.py
@@ -1,11 +1,12 @@
 class Solution:
-    def findErrorNums(self, nums: List[int]) -> List[int]:
+    def findErrorNums_array(self, nums: List[int]) -> List[int]:
         indices = [0] * len(nums)
         for n in nums:
             indices[n - 1] += 1
-        return [j[1] for j in sorted([(x, i + 1) for i, x in enumerate(indices) if x != 1], reverse=True)]
+        return [j[1] for j in sorted([(x, i + 1) for i, x in enumerate(indices) 
+                                      if x != 1], reverse=True)]
         
-    def findErrorNums_math(self, nums: List[int]) -> List[int]:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
         set_sum = sum(set(nums))
         new_num = sum(nums) - set_sum
         missing_num = len(nums) * (len(nums) + 1) // 2 - set_sum
