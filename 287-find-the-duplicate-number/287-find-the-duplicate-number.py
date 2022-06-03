2a4f2ecb8103a9ca3cf2b448afb338a943683b72
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-06-03 18:04:12 +0530

    Time: 733 ms (68.20%), Space: 34.4 MB (7.52%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,5 +1,4 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        
         set_nums = set(nums)
         return (sum(nums) - sum(set_nums)) // (len(nums) - len(set_nums))
