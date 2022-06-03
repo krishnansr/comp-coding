9fbd059c36ec7ee24d6608506a11b3a9485e9a30
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-06-03 13:19:53 +0530

    Time: 45 ms (67.65%), Space: 14 MB (30.24%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        size = 0
+        _node = head
+        while _node:
+            size += 1
+            _node = _node.next
+        
+        try:
+            k %= size  # to avoid needless iterations, redcue k to {0, len}
+            if k == 0:
+                raise Exception
+        except:
+            # EAFP is better than LBYL paradigm
+            return head
+        
+        _node = head
+        fast_node = head
+        for _ in range(k):
+            fast_node = fast_node.next
+            
+        while fast_node and fast_node.next:
+            fast_node = fast_node.next
+            _node = _node.next
+        
+        _node.next, fast_node.next, head = None, head, _node.next
+
+        return head
