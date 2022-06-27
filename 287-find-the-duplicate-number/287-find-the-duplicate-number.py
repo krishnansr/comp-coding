92231ee84d2d712bfb200e0e19b7b911947c5c55
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-06-27 20:25:10 +0530

    Time: 1521 ms (5.01%), Space: 27.9 MB (55.38%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,4 +1,21 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        set_nums = set(nums)
-        return (sum(nums) - sum(set_nums)) // (len(nums) - len(set_nums))
+        # problem can be re-imagined like 
+        # https://leetcode.com/problems/linked-list-cycle-ii/
+        slow = nums[0]
+        fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        fast = nums[0]  # move x steps to get to actual duplicate node
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
+        
+    def findDuplicate_extraspace(self, nums: List[int]) -> int:
+        return sum(nums) - sum(set(nums))
