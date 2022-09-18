6aa6d7b6b62c59ba7724397a3c418cfd98e0704e
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-09-18 08:34:59 +0530

    Time: 56 ms (71.91%), Space: 15.4 MB (76.42%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -9,17 +9,23 @@ class Solution:
         if root is None:
             return 0
 
-        max_depth = 0
-        stack = [(root, 1)]
+        level_node_counter = 1  # total number of nodes in a level
+        level_counter = 0 # total number of levels
+        stack = [root]
         while stack:
-            _node, curr_depth = stack.pop()
-            max_depth = max(max_depth, curr_depth)
-
+            _node = stack.pop(0)
+            level_node_counter -= 1
+            
             if _node.left:
-                stack.append((_node.left, curr_depth + 1))
+                stack.append(_node.left)
             if _node.right:
-                stack.append((_node.right, curr_depth + 1))        
-        return max_depth
+                stack.append(_node.right)        
+
+            if level_node_counter == 0:  # seen all nodes at current level
+                level_counter += 1  # increase level count
+                level_node_counter = len(stack)  # set num nodes count in next level
+                
+        return level_counter
         
     def maxDepth_bfs(self, root: Optional[TreeNode]) -> int:
         if root is None:
