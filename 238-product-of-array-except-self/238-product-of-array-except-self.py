c5a054a0871500c88b540a2b7a1825a424604b1f
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-09-23 07:23:45 +0530

    Time: 250 ms (88.87%), Space: 22.7 MB (13.25%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -0,0 +1,19 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = [1] * len(nums)
+        ans2 = [1] * len(nums)
+
+        for i in range(len(nums) - 1):
+            ans[i + 1] = ans[i]* nums[i]
+
+        for i in range(len(nums) - 1):
+            ans2[i + 1] = ans2[i] * nums[len(nums) - 1 - i]
+        
+        return [x*y for x, y in zip(ans, ans2[::-1])]
+            
+        
+    def productExceptSelf_np(self, nums: List[int]) -> List[int]:
+        # MLE
+        prod_array = np.vstack([np.full(len(nums), n) for n in nums])
+        np.fill_diagonal(prod_array, 1)
+        return np.prod(prod_array, axis=0).tolist()
