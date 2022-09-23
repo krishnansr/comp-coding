2be9e6807e65d019267c17de1c9e62ff5b447eb4
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-09-23 10:12:43 +0530

    Time: 48 ms (56.91%), Space: 13.7 MB (99.07%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,5 +1,6 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        # sol: https://leetcode.com/problems/spiral-matrix/discuss/20571/1-liner-in-Python-%2B-Ruby
         if matrix:
             first_row = matrix.pop(0)
             return first_row + self.spiralOrder([list(x) for x in zip(*matrix)][::-1])
