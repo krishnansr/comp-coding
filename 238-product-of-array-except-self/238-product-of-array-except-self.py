2194afcdd57963523360cd7863cd7c84b022d2f9
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-09-23 07:31:33 +0530

    Time: 591 ms (12.66%), Space: 21.4 MB (58.51%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,6 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         ans = [1] * len(nums)
+        suffix = prefix = 1
+        for i in range(len(nums)):
+            ans[i] *= prefix
+            ans[- i - 1] *= suffix
+            
+            prefix *= nums[i]
+            suffix *= nums[-i - 1]
+            
+        return ans
+        
+        
+    def productExceptSelf_2pass(self, nums: List[int]) -> List[int]:
+        ans = [1] * len(nums)
         ans2 = [1] * len(nums)
 
         for i in range(len(nums) - 1):
@@ -12,8 +25,8 @@ class Solution:
         return [x*y for x, y in zip(ans, ans2[::-1])]
             
         
-    def productExceptSelf_np(self, nums: List[int]) -> List[int]:
-        # MLE
-        prod_array = np.vstack([np.full(len(nums), n) for n in nums])
-        np.fill_diagonal(prod_array, 1)
-        return np.prod(prod_array, axis=0).tolist()
+    # def productExceptSelf_np(self, nums: List[int]) -> List[int]:
+    #     # MLE
+    #     prod_array = np.vstack([np.full(len(nums), n) for n in nums])
+    #     np.fill_diagonal(prod_array, 1)
+    #     return np.prod(prod_array, axis=0).tolist()
