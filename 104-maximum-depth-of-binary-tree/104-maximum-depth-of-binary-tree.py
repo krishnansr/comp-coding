4e5a311edaa4702b05394379fb9dc66095a2aeb0
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-10-03 01:27:49 +0530

    Time: 52 ms (80.05%), Space: 15.3 MB (81.09%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -12,20 +12,20 @@ class Solution:
 
         level_node_counter = 1  # total number of nodes in a level
         level_counter = 0 # total number of levels
-        stack = [root]
-        while stack:
-            _node = stack.pop(0)
+        queue = [root]
+        while queue:
+            _node = queue.pop(0)
             level_node_counter -= 1
-            
+
             if _node.left:
-                stack.append(_node.left)
+                queue.append(_node.left)
             if _node.right:
-                stack.append(_node.right)        
+                queue.append(_node.right)
 
             if level_node_counter == 0:  # seen all nodes at current level
                 level_counter += 1  # increase level count
-                level_node_counter = len(stack)  # set num nodes count in next level
-                
+                level_node_counter = len(queue)  # set num nodes count in next level
+
         return level_counter
     
     def maxDepth_rec(self, root: Optional[TreeNode]) -> int:
@@ -38,15 +38,15 @@ class Solution:
         # dfs traversal
         if root is None:
             return 0
-        
+
         max_depth = 0
-        queue = [(root, 1)]
-        while queue:
-            _node, curr_depth = queue.pop()
+        stack = [(root, 1)]
+        while stack:
+            _node, curr_depth = stack.pop()
             max_depth = max(max_depth, curr_depth)
-            
+
             if _node.left:
-                queue.append((_node.left, curr_depth + 1))
+                stack.append((_node.left, curr_depth + 1))
             if _node.right:
-                queue.append((_node.right, curr_depth + 1))
+                stack.append((_node.right, curr_depth + 1))
         return max_depth
