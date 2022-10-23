8f8ac41fc28f9585c825a9af30da6641e7ab9844
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2022-10-23 09:21:07 +0530

    Time: 3367 ms (5.06%), Space: 70 MB (5.05%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
@@ -2,8 +2,8 @@ class Solution:
     def minIncrementForUnique(self, nums: List[int]) -> int:
         # using Union find - https://leetcode.com/problems/minimum-increment-to-make-array-unique/solutions/197687/java-c-python-straight-forward/
         roots = {}
+        
         def find(x):
             roots[x] = find(roots[x] + 1) if x in roots else x
             return roots[x]
-
         return sum(find(n) - n for n in nums)
