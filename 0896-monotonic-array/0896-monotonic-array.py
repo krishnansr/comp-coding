36f3a08521b48c20aff438018c77619b14a6ccf6
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-01-17 06:14:11 +0530

    Time: 1010 ms (84.27%), Space: 28.2 MB (40.83%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -1,5 +1,16 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
+        if nums[-1] > nums[0]:  # increasing order
+            cmp_fn = lambda x, y: x > y
+        else:
+            cmp_fn = lambda x, y: x < y
+
+        for i in range(1, len(nums)):
+            if cmp_fn(nums[i - 1], nums[i]):
+                return False
+        return True
+        
+    def isMonotonic_branched(self, nums: List[int]) -> bool:
         monotonic = True
         dir_flag = 0
         
