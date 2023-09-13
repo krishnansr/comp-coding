155605705b8f502643af25d9017a8e9d55843423
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-09-12 23:32:56 -0400

    Update 0268-missing-number.py

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,3 +1,7 @@
 class Solution:
+    def missingNumber_bitwise(self, nums: List[int]) -> int:
+        return reduce(lambda i, j: i^j, nums + list(range(len(nums)+1)), 0)
+    
     def missingNumber(self, nums: List[int]) -> int:
-        return len(nums) * (len(nums) + 1) // 2 - sum(nums)
+        num_range = len(nums)
+        return (num_range*(num_range+1))//2 - sum(nums)
