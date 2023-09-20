bbe21852b2bb4278d5175eaf5058f21bf4d342b4
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-09-20 18:23:50 -0400

    Time: 68 ms (58.63%), Space: 17.7 MB (12.82%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        subtrahend_map = {}
+        for i, n in enumerate(nums):
+          subtrahend = target - n
+          
+          if subtrahend in subtrahend_map:
+            return [subtrahend_map[subtrahend], i]
+          subtrahend_map[n] = i
+        return [-1, -1]
