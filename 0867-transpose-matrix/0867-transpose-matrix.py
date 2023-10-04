c0b77cc3a389df7d05cb3cac8479bde0618fd176
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-10-04 14:03:06 -0400

    Time: 66 ms (91.28%), Space: 17.2 MB (58.45%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        trans = [[] for _ in range(len(matrix[0]))]
+        for row in matrix:
+            for i in range(len(row)):
+                trans[i].append(row[i])
+        return trans
+        
+    def transpose_square(self, matrix: List[List[int]]) -> List[List[int]]:
+        for r in range(len(matrix)):
+            for c in range(r + 1, len(matrix[0])):
+                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+        return matrix
