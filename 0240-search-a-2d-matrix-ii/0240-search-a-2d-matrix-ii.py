b0b6950eeb34f0d2f018ca823e8d3300bc2ab7e6
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-05 03:28:40 -0500

    Time: 156 ms (52.66%), Space: 22.8 MB (35.67%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,12 +1,12 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # O(m + n) solution. Trick is to start from top-right corner
+        # O(m + n) solution. Trick is to start from top-right corner.
         # explanation in https://leetcode.com/problems/search-a-2d-matrix-ii/discuss/332356/Python-O(m%2Bn)-Linear-Search-from-Top-Right-Corner
         
         num_rows = len(matrix)
         num_cols = len(matrix[0])
 
-        # Start from top-right corner.
+        # Start from top-right corner
         r, c = 0, num_cols - 1
         
         while r < num_rows and c >= 0:
