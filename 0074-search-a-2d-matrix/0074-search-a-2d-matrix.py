9990de409cd56bd30b460859dd097a22254c213e
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-05 01:35:30 -0500

    Time: 52 ms (43.78%), Space: 16.9 MB (33.30%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,36 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        num_rows = len(matrix)
+        num_cols = len(matrix[0])
+
+        # Search which row might contain the value.
+        l, r = 0, num_rows - 1
+        if num_rows > 1:
+            while l <= r:
+                mid = (l + r) // 2
+                if matrix[mid][-1] < target:
+                    l = mid + 1
+                elif matrix[mid][-1] >  target:
+                    r = mid - 1
+                else:  # Exact match.
+                    return True
+            row = min(l, num_rows - 1)
+        else:
+            row = 0
+        # TODO possible assert statements for check
+        print(l, r, row)
+        
+        if num_cols > 1:
+            l, r = 0, num_cols - 1
+            while l <= r:
+                mid = (l + r) // 2
+                if matrix[row][mid] < target:
+                    l = mid + 1
+                elif matrix[row][mid] > target:
+                    r = mid - 1
+                else:  # Exact match.
+                    return True
+        elif matrix[row][0] == target:
+            return True
+        
+        return False
