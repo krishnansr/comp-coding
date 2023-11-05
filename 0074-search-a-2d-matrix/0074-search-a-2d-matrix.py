87e4c7bafad7613972bfffa1617d23f13a342657
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-05 02:13:00 -0500

    Time: 44 ms (89.31%), Space: 16.8 MB (33.30%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,6 +3,27 @@ class Solution:
         num_rows = len(matrix)
         num_cols = len(matrix[0])
 
+        # Binary search on the matrix by assuming it’s an array.
+        l, r = 0, num_rows * num_cols - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            row, col = divmod(mid, num_cols)
+            if matrix[row][col] < target:
+                l = mid + 1
+            elif matrix[row][col] >  target:
+                r = mid - 1
+            else:  # Exact match.
+                return True
+        
+        return False
+
+        
+    def searchMatrix_sep(self, matrix: List[List[int]], target: int) -> bool:
+
+        num_rows = len(matrix)
+        num_cols = len(matrix[0])
+
         # Search which row might contain the value.
         l, r = 0, num_rows - 1
         while l <= r:
