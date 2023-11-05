f30a1d539f30c3d3b0f935d48ec150ff687603a6
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-05 01:36:38 -0500

    Time: 56 ms (17.13%), Space: 16.8 MB (33.30%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -5,32 +5,26 @@ class Solution:
 
         # Search which row might contain the value.
         l, r = 0, num_rows - 1
-        if num_rows > 1:
-            while l <= r:
-                mid = (l + r) // 2
-                if matrix[mid][-1] < target:
-                    l = mid + 1
-                elif matrix[mid][-1] >  target:
-                    r = mid - 1
-                else:  # Exact match.
-                    return True
-            row = min(l, num_rows - 1)
-        else:
-            row = 0
+        while l <= r:
+            mid = (l + r) // 2
+            if matrix[mid][-1] < target:
+                l = mid + 1
+            elif matrix[mid][-1] >  target:
+                r = mid - 1
+            else:  # Exact match.
+                return True
+        
+        row = min(l, num_rows - 1)
         # TODO possible assert statements for check
-        print(l, r, row)
-        
-        if num_cols > 1:
-            l, r = 0, num_cols - 1
-            while l <= r:
-                mid = (l + r) // 2
-                if matrix[row][mid] < target:
-                    l = mid + 1
-                elif matrix[row][mid] > target:
-                    r = mid - 1
-                else:  # Exact match.
-                    return True
-        elif matrix[row][0] == target:
-            return True
-        
+
+        l, r = 0, num_cols - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if matrix[row][mid] < target:
+                l = mid + 1
+            elif matrix[row][mid] > target:
+                r = mid - 1
+            else:  # Exact match.
+                return True
+
         return False
