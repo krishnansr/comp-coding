5805dff47b76eb9dd9a69e6d4a447d3b3bc97ac4
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-07 00:38:28 -0500

    Time: 877 ms (83.84%), Space: 16.7 MB (41.62%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,15 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # So many edge cases to handle in this bottom-up DP solution.
+        # O(T.N) where T is the amount
+        # Edge cases: 1. amount not matching with coins
+        # 2. 0 amount, 3. 0 coins, 4. starting coin is more than amount
+        # 5. ending coin is more than amount 6. happy path
+        
         if amount is 0:
             return 0
 
         dp = [0] * amount
         for coin in coins:
-            if coin - 1 < amount:
-                dp[coin - 1] = 1  # since we already have that denomination
+            if coin - 1 < amount:  # if amount is less than all coins.
+                dp[coin - 1] = 1  # since we already have that denomination.
         
         for i in range(amount):
-            # curr_amount = i + 1
             if dp[i] != 0:
                 continue  # already set
             
@@ -20,5 +25,4 @@ class Solution:
             else:
                 dp[i] = -1
         
-        print(dp)
         return dp[-1]
