8ca1fc462ec04f0a65a2d70d4680fe5372aed19b
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-06 23:04:28 -0500

    Time: 608 ms (48.36%), Space: 30.4 MB (85.35%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,5 +1,18 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        # kadane's algorithm
+        
+        max_sum = float('-inf')
+        curr_sum = 0
+        for n in nums:
+            curr_sum = max(0, curr_sum)  # additional step than regular max sum
+            curr_sum += n
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
+    
+    
+    def maxSubArray_understandable(self, nums: List[int]) -> int:
+        # same as above but more understandable and not concise
         max_sum = float('-inf')	
         curr_sum = 0
         for i, n in enumerate(nums):
@@ -8,6 +21,5 @@ class Solution:
             else:
                 curr_sum += n
             max_sum = max(max_sum, curr_sum)
-
         return max_sum
 
