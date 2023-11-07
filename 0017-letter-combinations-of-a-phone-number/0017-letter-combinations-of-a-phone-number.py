6881e35c58fe7469958a1b925cee84e91a1f3103
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-07 02:33:10 -0500

    Time: 28 ms (97.68%), Space: 16.2 MB (88.01%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -3,6 +3,22 @@ class Solution:
                  '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
     
     def letterCombinations(self, digits: str) -> List[str]:
+        # backtracking solution
+        words = []
+        
+        def backtrack(i: int, substring) -> None:
+            if i == len(digits):
+                words.append(substring)
+                return
+            else:
+                for _char in self.digit_map[digits[i]]:
+                    backtrack(i + 1, substring + _char)
+        
+        if digits:  # to handle empty string case
+            backtrack(0, '')
+        return words
+        
+    def letterCombinations_dfs(self, digits: str) -> List[str]:
         words = []
         if not digits:
             return words
