54cc5f9dd8181ecdd00a34c9bc2330653ab8ad82
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-12 03:56:21 -0500

    Time: 56 ms (6.49%), Space: 16.7 MB (9.18%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,25 +1,5 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
-
-        while l <= r:
-            mid = (l + r) // 2
-            if target == nums[mid]:
-                return mid
-
-            left_half_is_sorted = nums[l] <= nums[mid]
-            target_in_left_half = nums[l] <= target <= nums[mid]
-            target_in_right_half = nums[mid] <= target <= nums[r]
-            
-            if left_half_is_sorted and target_in_left_half:
-                r = mid - 1
-            elif (not left_half_is_sorted) and (not target_in_right_half):
-                r = mid - 1
-            else:
-                l = mid + 1
-        return -1
-        
-    def search_double_bin_search(self, nums: List[int], target: int) -> int:
         # Find the pivot index in O(log N) time.
         l, r = 0, len(nums) - 1
         while l <= r:
@@ -44,4 +24,24 @@ class Solution:
             else:
                 return mid
             
+        return -1
+    
+    def search_single_binsearch(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+
+            left_half_is_sorted = nums[l] <= nums[mid]
+            target_in_left_half = nums[l] <= target <= nums[mid]
+            target_in_right_half = nums[mid] <= target <= nums[r]
+            
+            if left_half_is_sorted and target_in_left_half:
+                r = mid - 1
+            elif (not left_half_is_sorted) and (not target_in_right_half):
+                r = mid - 1
+            else:
+                l = mid + 1
         return -1
