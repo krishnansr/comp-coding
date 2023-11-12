700886c795c2612337ab2a1b4297d470aeec65cc
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-12 03:21:28 -0500

    Time: 43 ms (82.49%), Space: 16.8 MB (9.18%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,28 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # Find the pivot index in O(log N) time.
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] < nums[r]:  # Right half is ordered.
+                r = mid
+            else:
+                l = mid + 1
+            
+        if target > nums[-1]:  # Target could be in larger half.
+            l, r = 0, mid - 1
+        else:  # Target could be in bigger half.
+            l, r = mid, len(nums)
+        
+        print(l, r, nums[l: r + 1])
+        # Find the number in O(log N) time.
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                return mid
+            
+        return -1
