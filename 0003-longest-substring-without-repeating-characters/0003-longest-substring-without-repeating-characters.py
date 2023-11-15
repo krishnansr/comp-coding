799f61a1439fd6389455ac5aa41f959ead550e74
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-15 15:21:16 -0500

    Time: 70 ms (32.69%), Space: 16.3 MB (82.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,5 +1,6 @@
 class Solution:
     def lengthOfLongestSubstring(self, string: str) -> int:
+        # Explanation from https://youtu.be/EM8IgIIiOdY?t=154
         char_set = set()
         max_length = 0
         
@@ -7,12 +8,29 @@ class Solution:
         while r < len(string):
             
             if string[r] not in char_set:
+                # New char, expand the sliding window.
                 char_set.add(string[r])
                 r += 1
                 max_length = max(max_length, r - l)
             else:
-                # Char is in set, need to remove it before expanding sliding window.
+                # Duplicate char, shrink the sliding window.
+                # Keep moving l up until duplicate of r is removed.
                 char_set.remove(string[l])
                 l += 1
             
+        return max_length
+
+    def lengthOfLongestSubstring_hashmap(self, s: str) -> int:
+        char_dict = dict()
+        seq_start = -1
+        max_length = 0
+        
+        for ind, char in enumerate(s):
+            if char in char_dict and seq_start <= char_dict[char]:
+                seq_start = char_dict[char]
+            else:
+                max_length = max(max_length, ind - seq_start)
+            
+            char_dict[char] = ind
+        
         return max_length
