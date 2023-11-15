972343ef20c903b633068ed83e36800e6b50b656
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-15 16:15:42 -0500

    Time: 47 ms (95.91%), Space: 16.5 MB (8.29%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,7 +1,27 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # Another solution using Dynamic Programming
+        # https://youtu.be/EM8IgIIiOdY?t=337
+
+        dp = [[] for _ in range(target + 1)]
+        dp[0].append([])  # Initialize 0 to Null list.
+
+        for cand in candidates:  # Runs for 2, 3, 5.
+            for i in range(cand, target + 1):  # Runs for 2 -> 8, 3 -> 8, 5 -> 8
+                for combo in dp[i - cand]:
+                    # Only extends if there's a previous combination in dp[i - cand].
+                    dp[i].append(combo + [cand])
+        
+        return dp[-1]
+    
+    
+    def combinationSum_backtrack(self, candidates: List[int], target: int) -> List[List[int]]:
         combinations = []
 
+        # Depth-First search is a specific form of backtracking 
+        # related to searching tree structures.
+        # Backtracking is a more general purpose algorithm.
+        # Backtracking is usually implemented as DFS plus search pruning
         def dfs(nums, target, path):
             if target == 0:
                 combinations.append(path)
