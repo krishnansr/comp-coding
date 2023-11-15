d74affcea97c56dbd2385e1b08a5b1f5b78b4500
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-15 03:06:30 -0500

    Time: 53 ms (8.54%), Space: 16.7 MB (14.46%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,15 +2,18 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         all_permutations = []
 
-        stack = [(nums, [])]  # Stack contains leftover_numbers, resultant_path.
-        while stack:
-            curr_nums, curr_path = stack.pop(0)
+        # Can solve using either BFS or DFS.
+        # Time complexity: O(E + V) in graph which is O(N!*N).
+        # Space complexity: O(N!).
+        queue = [(nums, [])]  # Stack contains leftover_numbers, resultant_path.
+        while queue:
+            curr_nums, curr_path = queue.pop(0)
             if not curr_nums:  # No nums to iterate on.
                 all_permutations.append(curr_path)
             
             for i in range(len(curr_nums)):
                 updated_nums = curr_nums[:i] + curr_nums[i + 1:]
                 updated_path = curr_path + [curr_nums[i]]
-                stack.append((updated_nums, updated_path))
+                queue.append((updated_nums, updated_path))
         
         return all_permutations                
