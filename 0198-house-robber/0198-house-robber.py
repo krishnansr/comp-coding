90ee8a57afe4797c9abdd70d942967ecc3285dcd
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-15 05:10:22 -0500

    Time: 46 ms (13.14%), Space: 16.2 MB (74.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,7 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         # detailed explanation in https://leetcode.com/problems/house-robber/solutions/156523/from-good-to-great-how-to-approach-most-of-dp-problems/
-        # similar to fibonacci (or) climbing 1-2 steps in ladder.
+        # similar to fibonacci (or) climbing 1-2 steps in ladder
 
         prev_max = 0
         max_money = 0
