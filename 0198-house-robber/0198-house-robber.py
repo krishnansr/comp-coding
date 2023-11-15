ebddb87c7bc3db3d8c6cdb500e51085fcf44d983
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-15 05:09:54 -0500

    Time: 42 ms (37.33%), Space: 16.4 MB (12.40%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,9 +3,9 @@ class Solution:
         # detailed explanation in https://leetcode.com/problems/house-robber/solutions/156523/from-good-to-great-how-to-approach-most-of-dp-problems/
         # similar to fibonacci (or) climbing 1-2 steps in ladder
 
-        prev1 = 0
-        prev2 = 0
+        prev_max = 0
+        max_money = 0
         for n in nums:
-            prev1, prev2 = max(prev2 + n, prev1), prev1
+            max_money, prev_max = max(prev_max + n, max_money), max_money
             
-        return prev1
+        return max_money
