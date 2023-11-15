29d1860c598831144e8b16571c18c536a8182a36
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-15 04:56:09 -0500

    Time: 44 ms (31.40%), Space: 16.5 MB (45.49%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,5 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()  # Easier to skip duplicates once sorted.
+        power_set = []
+
+        # Solution using BFS, also works with DFS.
+        queue = [([], nums)]  # (path, rem_nums)
+        while queue:
+            path, rem_nums = queue.pop(0)
+            power_set.append(path)
+
+            for i, n in enumerate(rem_nums):
+                queue.append((path + [n], rem_nums[i + 1:]))
+
+        return power_set
+    
+    def subsets_binary_approach(self, nums: List[int]) -> List[List[int]]:
         power_set = []
         
         # Solution using BFS, also works with DFS.
