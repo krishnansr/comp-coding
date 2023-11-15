610ea9bcc6d8579b5705e78213866f23433e06e9
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-15 04:55:18 -0500

    Time: 39 ms (80.90%), Space: 16.5 MB (58.89%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()  # Easier to skip duplicates once sorted.
+        power_set = []
+
+        # Solution using BFS, also works with DFS.
+        queue = [([], nums)]  # (path, rem_nums)
+        while queue:
+            path, rem_nums = queue.pop(0)
+            power_set.append(path)
+
+            for i, n in enumerate(rem_nums):
+                if i > 0 and rem_nums[i] == rem_nums[i - 1]:
+                    continue
+                queue.append((path + [n], rem_nums[i + 1:]))
+
+        return power_set
