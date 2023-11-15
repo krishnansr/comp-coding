bec6487d2b3df6c74939f2ed4428e11db3a23c63
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-15 03:40:39 -0500

    Time: 41 ms (53.50%), Space: 16.6 MB (13.43%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        power_set = []
+        
+        queue = [([], 0)]
+        while queue:
+            subset, ind = queue.pop(0)
+            if ind == len(nums):
+                power_set.append(subset)
+            else:          
+                queue.append((subset, ind + 1))  # 0-case.
+                queue.append((subset + [nums[ind]], ind + 1))  # 1-case.
+            
+        return power_set
