e57833727a3522d67f1fe323635edb0af2bb7932
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-15 23:10:57 -0500

    Time: 46 ms (24.05%), Space: 16.4 MB (17.63%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,18 +1,6 @@
 class Solution:
     def reorganizeString(self, name: str) -> str:
-        # Simpler solution using sort -> Time is O(N log N).
-        if len(name) < 2:
-            return name
-        
-        ls = sorted(sorted(name), key= name.count) 
-        
-        h = len(ls) // 2
-        ls[1::2], ls[::2] = ls[:h], ls[h:]
-        
-        return ''.join(ls) if ls[-1] != ls[-2] else ''
-            
-    def reorganizeString_heap(self, name: str) -> str:
-        # Simpler solution using heaps.
+        # Simpler solution using heaps. It is O(N log A) where A is the size of the alphabet in the heap, since it's a constant, overall time is O(N).
         counter = dict()
         for char in name:
             counter[char] = counter.get(char, 0) + 1
@@ -32,3 +20,15 @@ class Solution:
             prev_cnt, prev_char = cnt, char  # keep remembering for next call.
         
         return res if len(res) == len(name) else ''
+
+    def reorganizeString_sort(self, name: str) -> str:
+        # Simpler solution using sort -> Time is O(N log N).
+        # Solution from https://leetcode.com/problems/reorganize-string/discuss/113435/4-lines-Python
+        if len(name) < 2:
+            return name
+        
+        ls = sorted(sorted(name), key= name.count) 
+        mid = len(ls) // 2
+        ls[1::2], ls[::2] = ls[:mid], ls[mid:]
+        
+        return ''.join(ls) if ls[-1] != ls[-2] else ''
