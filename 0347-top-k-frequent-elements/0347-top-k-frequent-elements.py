b6ff635893e92963463c2240589ca2f22a02e7ce
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-16 13:46:54 -0500

    Time: 96 ms (71.63%), Space: 21.2 MB (38.33%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,5 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Time is O(N).
+        counter = dict()
+        for num in nums:
+            counter[num] = counter.get(num, 0) + 1
+        
+        res = list()
+        for times in reversed(range(len(nums) + 1)):
+            res.extend(frq[times])
+            if len(res) >= k: return res[:k]
+
+        return res[:k]        
+        
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Time is O(k log N).
         counter = dict()
         for num in nums:
             counter[num] = counter.get(num, 0) + 1
