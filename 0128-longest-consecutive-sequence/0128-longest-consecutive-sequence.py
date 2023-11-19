49f61c7a02cdd47e47058cc8a2d6c3542fffdc32
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-19 00:08:09 -0500

    Time: 740 ms (37.10%), Space: 171 MB (5.08%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -0,0 +1,34 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # build an adjacency map in O(N) time.
+        
+        adj_map = dict()
+        for num in nums:
+            neighbors = set()
+            if num - 1 in adj_map:
+                adj_map[num - 1].add(num)
+                neighbors.add(num - 1)
+            if num + 1 in adj_map:
+                adj_map[num + 1].add(num)
+                neighbors.add(num + 1)
+
+            adj_map[num] = neighbors
+        
+        # Now traverse through neighbors and find biggest cluster (like clustering problem)
+        def dfs(num):
+            total = 0
+            for neighbor in adj_map[num]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    total += dfs(neighbor)
+            return total + 1
+        
+        visited = set()
+        max_cluster = 0
+        for num in nums:
+            if num not in visited:
+                visited.add(num)
+                curr_cluster  = dfs(num)
+                max_cluster = max(max_cluster, curr_cluster)
+
+        return max_cluster
