f1a1e13bea00ff27b19a6464b247d2dfcf5a324f
Sivaramakrishnan <48220489+krishnansr@users.noreply.github.com>
2023-11-29 02:06:29 -0500

    Time: 707 ms (55.53%), Space: 22.3 MB (78.32%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,26 +1,15 @@
 class Solution:
-    def distance_fn(self, point: List[int]) -> int:
-        return point[0] ** 2 + point[1] ** 2
+    def distance_fn(self, x: int, y: int) -> int:
+        return x ** 2 + y ** 2
     
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # Other optimal solutions usign quickselect / kd-tree might run in O(N)
-        # example: https://leetcode.com/problems/k-closest-points-to-origin/discuss/576025/Python-3-lines-kNN-search-using-kd-tree-(for-large-number-of-queries)
-        
-        # Using max-heaps takes about O(Nlogk)
-        max_heap = [(-self.distance_fn(points[i]), points[i]) for i in range(k)]
-        heapq.heapify(max_heap)
-        
-        for i in range(k, len(points)):
-            curr_dist = self.distance_fn(points[i])
-            if max_heap[0][0] < curr_dist:
-                heapq.heappushpop(max_heap, (-curr_dist, points[i]))
+        max_heap = [(-self.distance_fn(*points[0]), points[0])]
+
+        for point in points[1:]:
+            curr_dist = self.distance_fn(*point)
+            if len(max_heap) < k:
+                heapq.heappush(max_heap, (-curr_dist, point))
+            elif max_heap[0][0] < curr_dist:
+                heapq.heappushpop(max_heap, (-curr_dist, point))
         
         return [x[1] for x in max_heap]
-    
-    def kClosest_min_heap(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # Using min-heaps takes about O(N + KlogN).
-        min_heap = [(self.distance_fn(pt), pt) for pt in points]
-        heapq.heapify(min_heap)
-        
-        k_closest = [heapq.heappop(min_heap)[1] for _ in range(k)]
-        return k_closest
